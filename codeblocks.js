5ae637f7b0956b9710f6ca9919b47cd2b4e5822f
Jake Iwen <iwenx006@umn.edu>
2017-04-13 18:24:59 -0500

    first

---
+++ codeblocks.js
@@ -0,0 +1,19 @@
+var codeBlocks = document.getElementsByTagName("pre");
+console.log('codeblock', codeBlocks);
+
+codeBlocks.forEach()
+
+
+function insertAfter(newElement,targetElement) {
+    // target is what you want it to go after. Look for this elements parent.
+    var parent = targetElement.parentNode;
+
+    // if the parents lastchild is the targetElement...
+    if (parent.lastChild == targetElement) {
+        // add the newElement after the target element.
+        parent.appendChild(newElement);
+    } else {
+        // else the target has siblings, insert the new element between the target and it's next sibling.
+        parent.insertBefore(newElement, targetElement.nextSibling);
+    }
+}
