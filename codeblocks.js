793b70099cbf8b701c8441a2b8a3d3e5f1317d00
Jake Iwen <iwenx006@umn.edu>
2017-04-13 20:08:12 -0500

    button injected

--- codeblocks.js
+++ codeblocks.js
@@ -1,19 +1,25 @@
 var codeBlocks = document.getElementsByTagName("pre");
 console.log('codeblock', codeBlocks);
 
-codeBlocks.forEach()
+var selectBtn = document.createElement('span');
+selectBtn.innerHTML = '<button onclick="selectText()">Select Code Block</button>';
 
+console.log('selectBtn', selectBtn);
 
-function insertAfter(newElement,targetElement) {
-    // target is what you want it to go after. Look for this elements parent.
-    var parent = targetElement.parentNode;
+for (var i = 0; i < codeBlocks.length; i++) {
+  var parent = codeBlocks[i].parentNode;
+  console.log('parent', parent);
+  // if the parents lastchild is the codeBlocks[i]...
+  // parent.insertBefore(selectBtn.cloneNode(true), codeBlocks[i]);
+  if (parent.lastChild == codeBlocks[i]) {
+      // add the newElement after the target element.
+      parent.appendChild(selectBtn.cloneNode(true));
+  } else {
+      // else the target has siblings, insert the new element between the target and it's next sibling.
+      parent.insertBefore(selectBtn.cloneNode(true), codeBlocks[i].nextSibling);
+  }
+}
 
-    // if the parents lastchild is the targetElement...
-    if (parent.lastChild == targetElement) {
-        // add the newElement after the target element.
-        parent.appendChild(newElement);
-    } else {
-        // else the target has siblings, insert the new element between the target and it's next sibling.
-        parent.insertBefore(newElement, targetElement.nextSibling);
-    }
+function selectText() {
+  return 1;
 }
