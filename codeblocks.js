28e67294b4d75d2a7394aa500776a6161dc1400d
Jake Iwen <iwenx006@umn.edu>
2017-04-14 18:56:32 -0500

    css

--- codeblocks.js
+++ codeblocks.js
@@ -1,43 +1,36 @@
-const copyImg = chrome.runtime.getURL("copy.png");
-const copiedImg = chrome.runtime.getURL("copied.svg");
-var codeBlocks = document.getElementsByTagName("pre");
-var selectBtn = document.createElement('span');
-selectBtn.innerHTML =
-'<img src="' + copyImg + '" class="clip" data-clipboard-action="copy" />';
+const copyImg = chrome.runtime.getURL("img/copy.png");
+const copiedImg = chrome.runtime.getURL("img/copied.svg");
+const emptyImg = chrome.runtime.getURL("img/empty.svg");
+const codeBlocks = document.getElementsByTagName("pre");
+var selectBtn = document.createElement('div');
+selectBtn.className = "clip";
+selectBtn.setAttribute("data-clipboard-action", "copy");
 
 for (var i = 0; i < codeBlocks.length; i++) {
-  codeBlocks[i].className += " block" + i;
   var thisBtn = selectBtn.cloneNode(true);
-  thisBtn.firstChild.className += " clip" + i;
-  thisBtn.firstChild.setAttribute("data-clipboard-target", ".block" + i);
-
-  var parent = codeBlocks[i].parentNode;
-  if (parent.lastChild == codeBlocks[i]) {
-    // add newElement after target element.
-    parent.appendChild(thisBtn);
-  } else {
-    // else target has siblings, insert new element between target and next sibling.
-    parent.insertBefore(thisBtn, codeBlocks[i].nextSibling);
-  }
+  codeBlocks[i].className += " block" + i;
+  thisBtn.className += " clip" + i;
+  thisBtn.setAttribute("data-clipboard-target", ".block" + i);
+  codeBlocks[i].appendChild(thisBtn);
 }
 
 var clipboard = new Clipboard('.clip');
-  clipboard.on('success', function(e) {
-    console.info('Action:', e.action);
-    console.info('Text:', e.text);
-    console.info('Trigger:', e.trigger);
-    defaultImgs();
-    e.trigger.setAttribute("src", copiedImg);
-    console.info("e", e);
-    e.clearSelection();
-  });
-  clipboard.on('error', function(e) {
-    console.error('Action:', e.action);
-    console.error('Trigger:', e.trigger);
+clipboard.on('success', function(e) {
+  console.info('Action:', e.action);
+  console.info('Text:', e.text);
+  console.info('Trigger:', e.trigger);
+  defaultImgs();
+  e.trigger.className = "clip copiedClip";
+  console.info("e", e);
+  e.clearSelection();
+});
+clipboard.on('error', function(e) {
+  console.error('Action:', e.action);
+  console.error('Trigger:', e.trigger);
 });
 
 function defaultImgs() {
   var imgs = document.getElementsByClassName("clip");
   for (var i = 0; i < imgs.length; i++)
-    imgs[i].setAttribute("src", copyImg);
+    imgs[i].className = "clip";
 }
