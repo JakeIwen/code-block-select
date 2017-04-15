ff4aa26de9c9274bfe8d9ab4ee19f91928fc7957
Jake Iwen <iwenx006@umn.edu>
2017-04-14 19:55:18 -0500

    update manifest, clean JS

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### CodeCopy
 A chrome browser extension to copy preformatted text.
+
+Simply click the icon that appears in the lower left side of the code block when it is hovered over. The code from the block will be copied to your clipboard.
+
+![Screenshot](https://github.com/JakeIwen/code-copy/blob/master/img/screencap.png)
--- codeblocks.js
+++ codeblocks.js
@@ -1,29 +1,31 @@
 const codeBlocks = document.getElementsByTagName("pre");
-var flag = false;
+if (codeBlocks.length) {
+  var flag = false;
 
-var selectBtn = document.createElement('div');
-selectBtn.className = "clip";
-selectBtn.setAttribute("data-clipboard-action", "copy");
+  var selectBtn = document.createElement('div');
+  selectBtn.className = "clip";
+  selectBtn.setAttribute("data-clipboard-action", "copy");
 
-for (var i = 0; i < codeBlocks.length; i++) {
-  codeBlocks[i].className += " block" + i;
-  var thisBtn = selectBtn.cloneNode(true);
-  thisBtn.setAttribute("data-clipboard-target", ".block" + i);
-  codeBlocks[i].appendChild(thisBtn);
+  for (var i = 0; i < codeBlocks.length; i++) {
+    codeBlocks[i].className += " block" + i;
+    var thisBtn = selectBtn.cloneNode(true);
+    thisBtn.setAttribute("data-clipboard-target", ".block" + i);
+    codeBlocks[i].appendChild(thisBtn);
+  }
+
+  var clipboard = new Clipboard('.clip');
+  clipboard.on('success', function(e) {
+    flag && document.getElementById("copiedClip").removeAttribute("id");
+    e.trigger.id = "copiedClip";
+    flag = true;
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+    e.clearSelection();
+  });
+
+  clipboard.on('error', function(e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+  });
 }
-
-var clipboard = new Clipboard('.clip');
-clipboard.on('success', function(e) {
-  flag && document.getElementById("copiedClip").removeAttribute("id");
-  e.trigger.id = "copiedClip";
-  flag = true;
-  console.info('Action:', e.action);
-  console.info('Text:', e.text);
-  console.info('Trigger:', e.trigger);
-  e.clearSelection();
-});
-
-clipboard.on('error', function(e) {
-  console.error('Action:', e.action);
-  console.error('Trigger:', e.trigger);
-});
